cac69147008f189d88c0bb7eaceb58b6258b4309
gjf2a <ferrer@hendrix.edu>
2020-07-30 10:26:07 -0500

    Genericized the distance metric.

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,13 @@
 use immutable_map::TreeMap;
 
 #[derive(Clone, Debug, Ord, PartialOrd, Eq, PartialEq)]
-pub struct LocationGraph<L: Copy+Clone+Ord+PartialOrd+Eq+PartialEq> {
-    distances: TreeMap<L,TreeMap<L,usize>>
+pub struct LocationGraph<L: Copy+Clone+Ord+PartialOrd+Eq+PartialEq, D:Copy+Clone+Ord+PartialOrd+Eq+PartialEq> {
+    distances: TreeMap<L,TreeMap<L,D>>
 }
 
-impl <L:Copy+Clone+Ord+PartialOrd+Eq+PartialEq> LocationGraph<L> {
-    pub fn new(distances: Vec<(L,L,usize)>) -> Self {
+impl <L:Copy+Clone+Ord+PartialOrd+Eq+PartialEq, D:Copy+Clone+Ord+PartialOrd+Eq+PartialEq>
+    LocationGraph<L,D> {
+    pub fn new(distances: Vec<(L,L,D)>) -> Self {
         let mut map_graph = LocationGraph {distances: TreeMap::new()};
         for distance in distances.iter() {
             map_graph.add(distance.0, distance.1, distance.2);
@@ -14,18 +15,18 @@ impl <L:Copy+Clone+Ord+PartialOrd+Eq+PartialEq> LocationGraph<L> {
         map_graph
     }
 
-    pub fn get(&self, start: L, end: L) -> Option<usize> {
+    pub fn get(&self, start: L, end: L) -> Option<D> {
         self.distances.get(&start)
             .and_then(|map| map.get(&end))
             .map(|d| *d)
     }
 
-    pub fn add(&mut self, m1: L, m2: L, distance: usize) {
+    pub fn add(&mut self, m1: L, m2: L, distance: D) {
         self.add_one_way(m1, m2, distance);
         self.add_one_way(m2, m1, distance);
     }
 
-    fn add_one_way(&mut self, start: L, end: L, distance: usize) {
+    fn add_one_way(&mut self, start: L, end: L, distance: D) {
         let updated = self.distances.get(&start)
             .unwrap_or(&TreeMap::new())
             .insert(end, distance);
